3f57382fd745273f485debc904239404afb058a2
Akshit Vig <akshitvig48@gmail.com>
2025-01-03 23:56:54 +0530

    fixed

--- src/components/airdrop.tsx
+++ src/components/airdrop.tsx
@@ -2,7 +2,6 @@ export const Airdrop = () => {
   return (
     <div className=" text-white">
       <input className=" rounded-sm p-1.5" type="text" placeholder="SOL" />
-      DDSD
       <button
         className="  rounded-sm bg-[#512da8] p-1.5 px-7"
         onClick={() => {
