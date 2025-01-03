cf2ccdc3fbf3007cb879663e1025fd21f4322a51
Akshit Vig <akshitvig48@gmail.com>
2025-01-03 23:54:41 +0530

    SA

--- src/components/airdrop.tsx
+++ src/components/airdrop.tsx
@@ -1,8 +1,9 @@
 export const Airdrop = () => {
   return (
-    <div>
-      <input type="text" placeholder="SOL" />
+    <div className=" text-white">
+      <input className=" rounded-sm p-1.5" type="text" placeholder="SOL" />
       <button
+        className="  rounded-sm bg-[#512da8] p-1.5 px-7"
         onClick={() => {
           alert("sended");
         }}
