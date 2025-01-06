0393314d30c6cad2c74433f0041076630d399bdd
Akshit Vig <akshitvig48@gmail.com>
2025-01-07 00:54:05 +0530

    sign fn finished

--- src/components/signmessage.tsx
+++ src/components/signmessage.tsx
@@ -1,11 +1,13 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useRef } from "react";
+import { ed25519 } from "@noble/curves/ed25519";
+import bs58 from "bs58";
 
 export const Signmsg = () => {
   const { signMessage, publicKey } = useWallet();
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const signmsg = () => {
+  const signmsg = async () => {
     if (!publicKey) {
       alert("wallet not connected ");
       return;
@@ -14,6 +16,13 @@ export const Signmsg = () => {
       alert("wallet does not support message signing");
       return;
     }
+    const encodedmsg = new TextEncoder().encode(inputRef.current?.value);
+    const signature = await signMessage(encodedmsg);
+
+    if (!ed25519.verify(signature, encodedmsg, publicKey.toBytes())) {
+      alert("message signature invalid");
+    }
+    alert(`message signature ${bs58.encode(signature)}`);
   };
 
   return;
