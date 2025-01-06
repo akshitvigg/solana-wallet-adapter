80128e2e55290637f645a71bb0eaa3d592b5efbc
Akshit Vig <akshitvig48@gmail.com>
2025-01-07 00:44:40 +0530

    added init

--- src/components/signmessage.tsx
+++ src/components/signmessage.tsx
@@ -1,1 +1,22 @@
-export const Signmsg = () => {};
+import { useWallet } from "@solana/wallet-adapter-react";
+
+export const Signmsg = () => {
+  const { signMessage, publicKey } = useWallet();
+
+  const signmsg = () => {
+    if (!publicKey) {
+      alert("wallet not connected ");
+      return;
+    }
+    if (!signMessage) {
+      alert("wallet does not support message signing");
+      return;
+    }
+  };
+
+  return;
+  <div>
+    <input type="text" name="" id="" />
+    <button>Sign</button>
+  </div>;
+};
