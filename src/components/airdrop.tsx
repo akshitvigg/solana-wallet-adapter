83a32f0510d4d75aefc4b895f875ab1aad9a2382
Akshit Vig <akshitvig48@gmail.com>
2025-01-07 22:08:50 +0530

    font update

--- src/components/airdrop.tsx
+++ src/components/airdrop.tsx
@@ -25,19 +25,22 @@ export const Airdrop = () => {
   };
 
   return (
-    <div className=" text-white flex justify-center pt-8 mt-2">
-      <input
-        onChange={(e) => setInputVal(Number(e.target.value))}
-        className="py-3 pl-10 outline-none w-96 bg-transparent border-zinc-700 border-2  rounded-lg p-1.5"
-        type="text"
-        placeholder="SOL"
-      />
-      <button
-        className="text-white  ml-3 text-xl hover:bg-[#1a1f2e] font-bold rounded-lg bg-[#512da8]  px-12"
-        onClick={sendAirdrop}
-      >
-        Get
-      </button>
+    <div className=" text-white  pt-8 mt-2">
+      <p className=" text-center text-3xl font-bold"> Airdrop Some Token</p>
+      <div className=" text-white flex mt-4 justify-center">
+        <input
+          onChange={(e) => setInputVal(Number(e.target.value))}
+          className="py-3 pl-10 outline-none w-96 bg-transparent border-zinc-700 border-2  rounded-lg p-1.5"
+          type="text"
+          placeholder="SOL"
+        />
+        <button
+          className="text-white  ml-3 text-xl hover:bg-[#1a1f2e] font-medium rounded-lg bg-[#512da8]  px-12"
+          onClick={sendAirdrop}
+        >
+          Get
+        </button>
+      </div>
     </div>
   );
 };
