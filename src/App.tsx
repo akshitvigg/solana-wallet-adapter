5778932c685ee8070b4934859a2d804a9bfd9127
Akshit Vig <akshitvig48@gmail.com>
2025-01-08 19:00:47 +0530

    pc version almost done

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,12 @@ function App() {
     <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div className=" font-poppins  min-h-screen bg-[url(/lbg1.png)]  dark:bg-[url(/bg.png)] ">
+          <div className=" font-poppins  transition-all duration-200 min-h-screen bg-[url(/lbg1.png)]  dark:bg-[url(/bg.png)] ">
             <Navbar />
             <div className="flex pt-24 justify-center ">
               <div>
-                <p className=" dark:text-white text-black text-4xl font-bold">
-                  Best Wallet adapter you will ever use
+                <p className=" text-center dark:text-white text-zinc-800 text-5xl font-bold">
+                  Effortless Wallet Integration <br /> for Solana Users
                 </p>
                 <div className="pb-2 gap-6 pt-10 flex justify-center ">
                   <WalletMultiButton style={{ borderRadius: "8px" }} />
