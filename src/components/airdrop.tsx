885b02debe75ccbf4b2079e432134c7ea3678bac
Akshit Vig <akshitvig48@gmail.com>
2025-01-08 21:45:00 +0530

    reponsivness done

--- src/components/airdrop.tsx
+++ src/components/airdrop.tsx
@@ -26,16 +26,18 @@ export const Airdrop = () => {
 
   return (
     <div className=" text-zinc-700 dark:text-white ">
-      <p className=" text-center text-3xl font-bold"> Airdrop Some Token</p>
+      <p className=" text-center sm:text-3xl text-2xl font-bold">
+        Airdrop Some Token
+      </p>
       <div className="  flex mt-4 justify-center">
         <input
           onChange={(e) => setInputVal(Number(e.target.value))}
-          className="py-3 pl-10 outline-none w-96 bg-white backdrop-blur-xl dark:bg-zinc-800/70 dark:border-zinc-700 border-gray-300 border-2  rounded-lg "
+          className="py-3 pl-10 outline-none sm:w-96 w-56 bg-white backdrop-blur-xl dark:bg-zinc-800/70 dark:border-zinc-700 border-gray-300 border-2  rounded-lg "
           type="text"
           placeholder="SOL"
         />
         <button
-          className="ml-3 text-xl text-white hover:bg-[#1a1f2e] font-medium rounded-lg bg-[#512da8]  px-12"
+          className="sm:ml-3 ml-2 text-xl text-white hover:bg-[#1a1f2e] font-medium rounded-lg bg-[#512da8] px-10  sm:px-12"
           onClick={sendAirdrop}
         >
           Get
