d1dc3260dcd61afe68655b7713f738b169a3be1d
Akshit Vig <akshitvig48@gmail.com>
2025-01-08 18:18:01 +0530

    code neatly seperated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import solanaLogo from "/solanaLogo.png";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 import { Switch } from "@/components/ui/switch";
+import solWord from "/download.png";
+import sol from "/solanaLogoMark.png";
 
 export const Navbar = () => {
   const [isDarktheme, setDarktheme] = useState<boolean>(false);
@@ -14,6 +16,7 @@ export const Navbar = () => {
     <div className=" flex justify-center">
       <div className=" shadow-md flex justify-between  py-8 bg-white/20 dark:bg-zinc-900/40 border border-gray-300 dark:border-zinc-700 rounded-2xl backdrop-blur-xl  translate-y-5 w-[1000px] ">
         <img className="ml-10" src={solanaLogo} width={150} alt="" />
+
         <div className=" flex items-center gap-2  mr-10">
           <IconMoon color={` ${!isDarktheme ? "#14F195" : "gray"}`} />
           <Switch onClick={toggleTheme} />
