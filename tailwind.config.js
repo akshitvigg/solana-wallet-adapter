5458fe85c8e8b17621a01a5727699930cc6f4d5b
Akshit Vig <akshitvig48@gmail.com>
2025-01-08 13:51:33 +0530

    tailwind rounded error fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			poppins: [
-  				'Poppins'
-  			]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {},
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
